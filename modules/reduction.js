9c663544d0562bec04e21cf75aeaf76fe7317185
Azer Sadykhzadeh <51178055+sadykhzadeh@users.noreply.github.com>
2020-06-27 00:13:38 +0400

    joining 2 modules to each other

--- modules/reduction.js
+++ modules/reduction.js
@@ -1,6 +1,32 @@
-import {
-    verdicts
-} from "./modules/verdicts.js"
+const verdicts = [{
+        full: "WRONG_ANSWER",
+        short: "WA"
+    },
+    {
+        full: "TIME_LIMIT_EXCEEDED",
+        short: "TLE"
+    },
+    {
+        full: "COMPILATION_ERROR",
+        short: "CE"
+    },
+    {
+        full: "RUNTIME_ERROR",
+        short: "RE"
+    },
+    {
+        full: "CHALLENGED / HACKED",
+        short: "CHL"
+    },
+    {
+        full: "MEMORY_LIMIT_EXCEEDED",
+        short: "MLE"
+    },
+    {
+        full: "SKIPPED",
+        short: "IGNORED"
+    }
+]
 
 const reduction = (verdict) => {
     for (let i of verdicts) {
