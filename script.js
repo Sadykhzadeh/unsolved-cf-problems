de9ea363defa73ba74451217ac728ebe6e2f0662
Azer Sadykhzadeh <51178055+sadykhzadeh@users.noreply.github.com>
2020-07-08 00:19:41 +0400

    Added "Russian/English" localization
    With Query String the site remembers the language used by the user
    
    The "localStorage" technology is also used, which helps to remember the
    user's language and last entered handle, thus making it much easier to use website :+1:

--- script.js
+++ script.js
@@ -28,6 +28,59 @@ const verdicts = [{
     }
 ]
 
+const localize = [{
+    "en": {
+        searchHandleButton: "Search unsolved tasks",
+        showTagsChexBox: " Show problem's tags",
+        aboutButton: "About",
+        counterOfProblems: `😱 Count of problems: `,
+        nameTD: "Name ",
+        tagsTD: "Tags",
+        ratingTD: "Rating",
+        lastTD: "Last Verdict / Submit Link",
+        congratsText: "Congratulations, You haven't got any unsolved tasks! 🥳",
+        errorText: ":( Error 4xx: Perhaps this handle does not exist"
+    },
+    "ru": {
+        searchHandleButton: "Поиск нерешённых задач",
+        showTagsChexBox: " Показывать теги задач",
+        aboutButton: "О нас",
+        counterOfProblems: `😱 Количество задач: `,
+        nameTD: "Название ",
+        tagsTD: "Теги",
+        ratingTD: "Рейтинг",
+        lastTD: "Последний вердикт / Последняя отправка",
+        congratsText: "Поздравляю, У тебя нет нерешённых задач! 🥳",
+        errorText: ":( Ошибка 4xx: Возможно данного хендла не существует"
+    }
+}]
+
+const getParameterByName = (name, url) => {
+    url = (!url) ? window.location.href : url;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    return (!results) ? null : (!results[2]) ? '' : decodeURIComponent(results[2].replace(/\+/g, ' '))
+}
+
+let lang = (localStorage.lang == undefined && getParameterByName("lang") == undefined) ? "en" :
+    (getParameterByName("lang") != undefined) ? getParameterByName("lang") : localStorage.lang
+
+const onloadFunction = (lang) => {
+    if (localStorage.lang != lang) {
+        localStorage.lang = lang
+    }
+    document.getElementById("handle").value = (localStorage.handle != undefined) ? localStorage.handle : ""
+    document.getElementById("start").click()
+    document.getElementById("start").innerHTML = localize[0][`${lang}`].searchHandleButton
+    document.getElementById("doNotShowTags-label").innerHTML = localize[0][`${lang}`].showTagsChexBox
+    document.getElementById("about").innerHTML = localize[0][`${lang}`].aboutButton
+    document.getElementById("nameTD").innerHTML = localize[0][`${lang}`].nameTD
+    document.getElementById("tagsTD").innerHTML = localize[0][`${lang}`].tagsTD
+    document.getElementById("ratingTD").innerHTML = localize[0][`${lang}`].ratingTD
+    document.getElementById("lastTD").innerHTML = localize[0][`${lang}`].lastTD
+}
+
 const reduction = (verdict) => {
     for (let i of verdicts) {
         if (i.full.includes(verdict)) return [i.short, i.full]
@@ -42,70 +95,78 @@ startButton.addEventListener("keyup", (event) => {
     }
 });
 
-start.onclick = async function() {
-    document.getElementById("table-main").hidden = true
-    document.getElementById("counter").hidden = false
-    document.getElementById("table-list").innerHTML = ""
-    const handle = document.getElementById('handle').value
-    if (handle == "") {
-        document.getElementById("counter").hidden = true
-        return -1
-    }
-    document.getElementById("counter").innerHTML = "Loading..."
-    document.getElementById("start").disabled = true
-    document.getElementById("doNotShowTags").disabled = true
-    console.log(`Ok, handle is "${handle}"`)
-    const res = await fetch(`https://codeforces.com/api/user.status?handle=${handle}&lang=en`)
-    if (!res.ok) {
-        let error = ":( Error 4xx: Perhaps this handle does not exist"
-        console.log(error)
-        document.getElementById("counter").innerHTML = error
-    } else {
-        console.log("Yeah, CodeForces is working! ^_^")
-        let submition = await res.json()
-        submition = submition.result
-        let allNotOkSumbitions = {},
-            ArrOfunsolvedTasks = {},
-            unsolvedTasks = [];
-        console.log(`Finding ${handle}'s unsolved tasks...`)
-        for (let i in submition) {
-            if (submition[i].verdict === 'OK') {
-                allNotOkSumbitions[submition[i].problem.contestId + submition[i].problem.index] = submition[i]
-            }
-        }
-        for (let i in submition) {
-            if (allNotOkSumbitions[submition[i].problem.contestId + submition[i].problem.index] == null) {
-                if (!ArrOfunsolvedTasks[submition[i].problem.contestId + submition[i].problem.index])
-                    ArrOfunsolvedTasks[submition[i].problem.contestId + submition[i].problem.index] = submition[i]
-            }
-        }
-        allNotOkSumbitions = ""
-        for (let i in ArrOfunsolvedTasks) unsolvedTasks.push(ArrOfunsolvedTasks[i])
-        ArrOfunsolvedTasks = ""
-        console.log("Sorting problems by rating...")
-        unsolvedTasks.sort((q, w) => {
-            let a = (q.problem.rating == undefined) ? 10e5 : q.problem.rating,
-                b = (w.problem.rating == undefined) ? 10e5 : w.problem.rating
-            return a - b
-        })
+start.onclick = async() => {
+        document.getElementById("nameTD").innerHTML = localize[0][`${lang}`].nameTD
+        document.getElementById("table-main").hidden = true
+        document.getElementById("counter").hidden = false
         document.getElementById("table-list").innerHTML = ""
-        if (!unsolvedTasks.length) {
-            document.getElementById("counter").hidden = false
-            document.getElementById("counter").innerHTML = "Congratulations, You haven't got any unsolved tasks! 🥳"
-            console.log("You haven't got any unsolved tasks")
+        const handle = document.getElementById('handle').value
+        if (handle == "") {
+            document.getElementById("counter").hidden = true
+            return -1
+        }
+        localStorage.handle = handle
+        document.getElementById("counter").innerHTML = "Loading..."
+        document.getElementById("start").disabled = true
+        document.getElementById("doNotShowTags").disabled = true
+        console.log(`Ok, handle is "${handle}"`)
+        const res = await fetch(`https://codeforces.com/api/user.status?handle=${handle}&lang=${lang}`)
+        if (!res.ok) {
+            let error = localize[0][`${lang}`].errorText
+            localStorage.handle = ""
+            console.log(error)
+            document.getElementById("counter").innerHTML = error
         } else {
-            console.log(unsolvedTasks)
-            document.getElementById("table-main").hidden = false
-            document.getElementById("counter").innerHTML = `${handle} has got ${unsolvedTasks.length} unsolved problems! 😱`
+            console.log("Yeah, CodeForces is working! ^_^")
+            let submition = await res.json()
+            submition = submition.result
+            let allNotOkSumbitions = {},
+                ArrOfunsolvedTasks = {},
+                unsolvedTasks = [];
+            console.log(`Finding ${handle}'s unsolved tasks...`)
+            for (let i in submition) {
+                if (submition[i].verdict === 'OK') {
+                    allNotOkSumbitions[submition[i].problem.contestId + submition[i].problem.index] = submition[i]
+                }
+            }
+            for (let i in submition) {
+                if (allNotOkSumbitions[submition[i].problem.contestId + submition[i].problem.index] == null) {
+                    if (!ArrOfunsolvedTasks[submition[i].problem.contestId + submition[i].problem.index])
+                        ArrOfunsolvedTasks[submition[i].problem.contestId + submition[i].problem.index] = submition[i]
+                }
+            }
+            allNotOkSumbitions = ""
+            for (let i in ArrOfunsolvedTasks) unsolvedTasks.push(ArrOfunsolvedTasks[i])
+            ArrOfunsolvedTasks = ""
+            console.log("Sorting problems by rating...")
+            unsolvedTasks.sort((q, w) => {
+                let a = (q.problem.rating == undefined) ? 10e5 : q.problem.rating,
+                    b = (w.problem.rating == undefined) ? 10e5 : w.problem.rating
+                return a - b
+            })
+            document.getElementById("table-list").innerHTML = ""
+            if (!unsolvedTasks.length) {
+                document.getElementById("counter").hidden = false
+                document.getElementById("counter").innerHTML = localize[0][`${lang}`].congratsText
+                console.log("You haven't got any unsolved tasks :)")
+            } else {
+                console.log(unsolvedTasks)
+                document.getElementById("table-main").hidden = false
+                document.getElementById("counter").innerHTML = `${localize[0][`${lang}`].counterOfProblems} ${unsolvedTasks.length}`
             for (let i in unsolvedTasks) {
-                let linkToTask = (+unsolvedTasks[i].problem.contestId >= 100000) ? `https://codeforces.com/problemset/gymProblem/${unsolvedTasks[i].problem.contestId}/${unsolvedTasks[i].problem.index}` : `https://codeforces.com/contest/${unsolvedTasks[i].problem.contestId}/problem/${unsolvedTasks[i].problem.index}`,
+                let linkToTask = (+unsolvedTasks[i].problem.contestId >= 100000)
+                        ? `https://codeforces.com/problemset/gymProblem/${unsolvedTasks[i].problem.contestId}/${unsolvedTasks[i].problem.index}` 
+                        : `https://codeforces.com/contest/${unsolvedTasks[i].problem.contestId}/problem/${unsolvedTasks[i].problem.index}`,
                     slash = `${unsolvedTasks[i].problem.contestId}|${unsolvedTasks[i].problem.index}`,
                     linkToLastSubmit = `https://codeforces.com/contest/${unsolvedTasks[i].problem.contestId}/submission/${unsolvedTasks[i].id}`
                 document.getElementById('table-list').innerHTML += `
                 <tr style = "border-bottom: solid 1px white;">
                     <td><a href="${linkToTask}" target="_blank">${slash}</a></td>
                     <td><a href="${linkToTask}" target="_blank" id="problemName">${unsolvedTasks[i].problem.name}</a></td>
-                    <td>${(!document.getElementById('doNotShowTags').checked) ? "We don't show tags 🙈" : (unsolvedTasks[i].problem.tags.length) ? unsolvedTasks[i].problem.tags.join(", "): '-'}</td>
+                    <td>${(!document.getElementById('doNotShowTags').checked) 
+                            ? "We don't show tags 🙈" 
+                            : (unsolvedTasks[i].problem.tags.length) 
+                            ? unsolvedTasks[i].problem.tags.join(", "): '-'}</td>
                     <td title="Rating">${(unsolvedTasks[i].problem.rating == undefined) ? '-' : unsolvedTasks[i].problem.rating}</td>
                     <td title="${reduction(unsolvedTasks[i].verdict)[1]}">${reduction(unsolvedTasks[i].verdict)[0]}</td>
                     <td><a href="${linkToLastSubmit}" target="_blank">${unsolvedTasks[i].id}</a></td>
@@ -116,7 +177,9 @@ start.onclick = async function() {
     }
     document.getElementById("start").disabled = false
     document.getElementById("doNotShowTags").disabled = false
+    document.getElementById("nameTD").innerHTML += '<input type="text" id="searchTask" placeholder="Search...">'
     console.log("We did it!!")
+    searchTasks()
 }
 
 about.onclick = async function() {
@@ -128,13 +191,18 @@ about.onclick = async function() {
     }
     document.getElementById("table-list").innerHTML = `
         </br>
-        <a href="https://github.com/sadykhzadeh/unsolved-cf-problems/stargazers"style="margin-right:1%;"><img alt="GitHub stars" src="https://img.shields.io/github/stars/sadykhzadeh/unsolved-cf-problems?color=darkgreen&style=flat-square"></a><a href="https://github.com/sadykhzadeh/unsolved-cf-problems/issues"><img alt="GitHub issues" src="https://img.shields.io/github/issues/sadykhzadeh/unsolved-cf-problems?color=blue&style=flat-square"></a><h3><a href="https://github.com/sadykhzadeh/unsolved-cf-problems" target="_blank">Project's Github</a> |
+        <a href="https://github.com/sadykhzadeh/unsolved-cf-problems/stargazers"style="margin-right:1%;"> \
+        <img alt="GitHub stars" src="https://img.shields.io/github/stars/sadykhzadeh/unsolved-cf-problems?color=darkgreen&style=flat-square"> \
+        </a><a href="https://github.com/sadykhzadeh/unsolved-cf-problems/issues"> \
+        <img alt="GitHub issues" src="https://img.shields.io/github/issues/sadykhzadeh/unsolved-cf-problems?color=blue&style=flat-square"> \
+        </a><h3><a href="https://github.com/sadykhzadeh/unsolved-cf-problems" target="_blank">Project's Github</a> |
         by Azer Sadykhzadeh [<a href="https://github.com/sadykhzadeh" target="_blank">Github</a>]</h3>
     `
 }
 
 let searchTasks = () => {
-    let name = document.getElementById('searchTask')
+    try
+    {let name = document.getElementById('searchTask')
     name.addEventListener('keyup', () => {
         let whatWeSearch = name.value.toLowerCase(),
             taskList = document.querySelectorAll('#table-list tr');
@@ -146,6 +214,6 @@ let searchTasks = () => {
                 elem.style.display = 'none';
             }
         })
-    });
+    });} catch{};
 }
-searchTasks();
+searchTasks()
